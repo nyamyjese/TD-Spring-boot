53a41208263e1bf3b95d5dd444a3e73b07d82bba
nyamyjese <hei.jese.2@gmail.com>
2026-03-18 22:48:01 +0100

    initial commit

---
+++ src/main/java/com/example/tdspringboot/TdSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tdspringboot/TdSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tdspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
