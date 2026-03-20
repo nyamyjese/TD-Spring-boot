f8aa500431e1d40df8c0b39fa06fa52b172ee517
nyamyjese <hei.jese.2@gmail.com>
2026-03-20 08:19:33 +0100

    feat:add model for td2

---
+++ src/main/java/com/example/tdspringboot/model/Student.java
@@ -0,0 +1,17 @@
+package com.example.tdspringboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Student {
+    private String references;
+    private String firstName;
+    private String lastName;
+    private int age;
+}
