a25d97c91321bd538a95e2da17d74aa5bef7d1c5
nyamyjese <hei.jese.2@gmail.com>
2026-03-25 12:27:41 +0100

    feat:add the StudentValidator for the td4

---
+++ src/main/java/com/example/tdspringboot/validator/StudentValidator.java
@@ -0,0 +1,20 @@
+package com.example.tdspringboot.validator;
+
+import com.example.tdspringboot.exception.BadRequestException;
+import com.example.tdspringboot.model.Student;
+
+public class StudentValidator {
+    public void validate (Student student) {
+        if(student.getReference() == null || student.getReference().isBlank()) {
+            throw new BadRequestException("NewStudent.reference cannot be null");
+        }
+
+        if(student.getFirstName() == null || student.getFirstName().isBlank()) {
+            throw new BadRequestException("NewStudent.firstname cannot be null");
+        }
+
+        if(student.getLastName() == null || student.getLastName().isBlank()) {
+            throw new BadRequestException("NewStudent.lastname cannot be null");
+        }
+    }
+}
